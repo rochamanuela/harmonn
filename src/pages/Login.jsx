75af2021131feda62c610d4a9f393fbf5c224744
rochamanuela <rochadesouzamanuela@gmail.com>
2025-10-09 21:30:51 -0300

    try use tailwind css

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+        <h1 className="text-4xl font-bold text-blue-600">Funciona logoooooo</h1>
+    </div>
+  )
+}
